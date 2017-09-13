1be2c716ec31d156f9b4950461303b7a3d403443
scottlaw1 <slawrence@gmail.com>
2017-09-12 21:56:24 -0400

    Copied the Steve Smith example of how nulls break polymorphism.

---
+++ NullsBreakPolymorphismExample/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullsBreakPolymorphismExample
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateHired { get; set; }
+        public DateTime? DateTerminated { get; set; }
+
+        public int TenureInDays(DateTime? currentDate = null)
+        {
+            DateTime endDate = currentDate ?? DateTime.Now;
+            if (DateTerminated.HasValue)
+            {
+                endDate = DateTerminated.Value;
+            }
+            return (endDate - DateHired).Days;
+        }
+    }
+}
