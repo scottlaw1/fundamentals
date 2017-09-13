6b566c0e9a192c9d91e4ed8894df8fb8850e232f
scottlaw1 <slawrence@gmail.com>
2017-09-12 22:07:57 -0400

    Implemented and used extension method to handle the "null" employee.

---
+++ NullsBreakPolymorphismExample/EmployeeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NullsBreakPolymorphismExample
+{
+    public static class EmployeeExtensions
+    {
+        public static string ToDisplayString(this Employee employee)
+        {
+            if (employee != null)
+                return String.Format("{0} {1}: {2} days", employee.FirstName, employee.LastName, employee.TenureInDays());
+
+            return "Unknown employee";
+        }
+    }
+}
